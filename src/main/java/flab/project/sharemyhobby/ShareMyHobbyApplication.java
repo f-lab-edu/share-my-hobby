bd298f1826b3b688e418f45c8d6574e40f1ec876
Philip <pch4623@gmail.com>
2021-03-02 23:04:25 +0900

    [#1] 프로젝트 생성

---
+++ src/main/java/flab/project/sharemyhobby/ShareMyHobbyApplication.java
@@ -0,0 +1,13 @@
+package flab.project.sharemyhobby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareMyHobbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareMyHobbyApplication.class, args);
+	}
+
+}
