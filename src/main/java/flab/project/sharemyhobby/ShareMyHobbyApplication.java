1633f8b432769b14c2c75edd1aa04d38c4bf1070
Philip <pch4623@gmail.com>
2021-03-06 00:15:35 +0900

    들여쓰기 간격 Tab에서 4 spaces로 수정

--- src/main/java/flab/project/sharemyhobby/ShareMyHobbyApplication.java
+++ src/main/java/flab/project/sharemyhobby/ShareMyHobbyApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShareMyHobbyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShareMyHobbyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShareMyHobbyApplication.class, args);
+    }
 
 }
--- src/test/java/flab/project/sharemyhobby/ShareMyHobbyApplicationTests.java
+++ src/test/java/flab/project/sharemyhobby/ShareMyHobbyApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ShareMyHobbyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
