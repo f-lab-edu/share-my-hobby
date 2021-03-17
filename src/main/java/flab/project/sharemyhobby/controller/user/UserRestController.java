35cc8ac5902a1439b749d010a7aaacbfeaca8ee7
Philip <pch4623@gmail.com>
2021-03-17 23:06:15 +0900

    [#3] 유저 컨트롤러 추가 : 회원가입

---
+++ src/main/java/flab/project/sharemyhobby/controller/user/UserRestController.java
@@ -0,0 +1,27 @@
+package flab.project.sharemyhobby.controller.user;
+
+import flab.project.sharemyhobby.model.api.request.user.JoinRequest;
+import flab.project.sharemyhobby.model.api.response.user.JoinResponse;
+import flab.project.sharemyhobby.model.user.Email;
+import flab.project.sharemyhobby.model.user.User;
+import flab.project.sharemyhobby.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserRestController {
+
+    private final UserService userService;
+
+    @PostMapping(path = "/join")
+    public ResponseEntity<JoinResponse> join(@RequestBody JoinRequest joinRequest) {
+        User user = userService.join(new Email(joinRequest.getEmail()),  joinRequest.getNickname(), joinRequest.getPassword());
+        return ResponseEntity.ok(new JoinResponse(user));
+    }
+
+}
