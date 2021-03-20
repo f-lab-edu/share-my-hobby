6fc04a371f93bf68b690198b6883f1463c0ed51d
Philip <pch4623@gmail.com>
2021-03-20 14:30:47 +0900

    [#3] 테스트용 H2 DB 추가 연동

--- src/main/java/flab/project/sharemyhobby/config/DataSourceConfig.java
+++ src/main/java/flab/project/sharemyhobby/config/DataSourceConfig.java
@@ -13,6 +13,7 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
@@ -30,15 +31,17 @@ public class DataSourceConfig {
     }
 
     @Bean
-    public DataSource dataSource() {
+    @Profile("test")
+    public DataSource testDataSource() {
         DataSourceBuilder factory = DataSourceBuilder
                 .create()
-                .driverClassName("com.mysql.cj.jdbc.Driver")
-                .url("jdbc:mysql://localhost:3306/f-lab?autoreconnect=true&characterEncoding=UTF-8&serverTimezone=Asia/Seoul")
-                .username("root")
-                .password("gehlen10");
+                .driverClassName("org.h2.Driver")
+                .url("jdbc:h2:mem:test_sharemyhobby;MODE=MYSQL;DB_CLOSE_DELAY=-1");
         HikariDataSource dataSource = (HikariDataSource) factory.build();
-       return new Log4jdbcProxyDataSource(dataSource);
+        dataSource.setPoolName("TEST_H2_DB");
+        dataSource.setMinimumIdle(1);
+        dataSource.setMaximumPoolSize(1);
+        return new Log4jdbcProxyDataSource(dataSource);
     }
 
     @Bean
--- src/test/java/flab/project/sharemyhobby/service/user/UserServiceTest.java
+++ src/test/java/flab/project/sharemyhobby/service/user/UserServiceTest.java
@@ -1,32 +1,28 @@
 package flab.project.sharemyhobby.service.user;
 
-import flab.project.sharemyhobby.mapper.user.UserMapper;
 import flab.project.sharemyhobby.model.user.Email;
 import flab.project.sharemyhobby.model.user.User;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.extension.*;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@ActiveProfiles("test")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserServiceTest {
 
     private Logger log = LoggerFactory.getLogger(getClass());
 
-    @InjectMocks
+    @Autowired
     private UserService userService;
-
-    @Mock
-    private UserMapper userMapper;
 
     private Email email;
     private String name;
@@ -36,7 +32,7 @@ class UserServiceTest {
     void setUp() {
         email = new Email("test@gmail.com");
         name = "박찬호";
-        password = "1234";
+        password = "12345678";
     }
 
     @Test
@@ -46,6 +42,6 @@ class UserServiceTest {
         assertThat(user, is(notNullValue()));
         assertThat(user.getId(), is(notNullValue()));
         assertThat(user.getEmail(), is(email));
-        log.info("Inserted user: {}", user);
+        log.info("회원가입 : {}", user);
     }
 }
