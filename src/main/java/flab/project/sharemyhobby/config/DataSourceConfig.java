48868ca80aae115f4f5a12d7701aa0a9d613e5b2
Philip <pch4623@gmail.com>
2021-04-04 00:59:42 +0900

    [#3] 리뷰반영: YAML 파일 프로파일 나누기
    - develop/test 프로파일별로 구분
    - DataSourceConfig 파일에서 test 프로파일용 설정 코드 제거

--- src/main/java/flab/project/sharemyhobby/config/DataSourceConfig.java
+++ src/main/java/flab/project/sharemyhobby/config/DataSourceConfig.java
@@ -1,18 +1,15 @@
 package flab.project.sharemyhobby.config;
 
 import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
@@ -27,20 +24,6 @@ public class DataSourceConfig {
     @ConfigurationProperties(prefix = "spring.datasource.hikari")
     public HikariConfig hikariConfig() {
         return new HikariConfig();
-    }
-
-    @Bean
-    @Profile("test")
-    public DataSource testDataSource() {
-        DataSourceBuilder factory = DataSourceBuilder
-                .create()
-                .driverClassName("org.h2.Driver")
-                .url("jdbc:h2:mem:test_sharemyhobby;MODE=MYSQL;DB_CLOSE_DELAY=-1");
-        HikariDataSource dataSource = (HikariDataSource) factory.build();
-        dataSource.setPoolName("TEST_H2_DB");
-        dataSource.setMinimumIdle(1);
-        dataSource.setMaximumPoolSize(1);
-        return dataSource;
     }
 
     @Bean
