7786bc16eda2ef356a6573b17528fde6e1ff74d6
Philip <pch4623@gmail.com>
2021-04-04 12:17:06 +0900

    [#3] 리뷰반영: 테스트 수정
    - 테스트에 사용한 애노테이션 설명 주석으로 추가
    - 테스트 설명을 위해 메소드 이름 변경 및 @DisplayName 활용

--- src/test/java/flab/project/sharemyhobby/service/user/UserServiceTest.java
+++ src/test/java/flab/project/sharemyhobby/service/user/UserServiceTest.java
@@ -12,6 +12,19 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
+/*
+ * @SpringBootTest
+ *  - 통합 테스트를 제공하는 기본적인 Spring Boot 테스트 애노테이션
+ *  - @SpringBootApplication을 찾아 하위 Bean들을 스캔하며 빈으로 등록해 테스트에 필요한 의존성을 추가
+ * @ActiveProfiles("test")
+ *  - test 프로파일에 해당하는 환경 값이 설정되도록 지정
+ * @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+ *  - 테스트 인스턴스의 라이프 사이클을 클래스 단위로 지정함으로써 테스트 실행 중 단 하나의 인스턴스를 생성하여 사용
+ *  - @BeforeAll을 정적 메소드로 지정하지 않아도 됨
+ * @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+ *  - 각 테스트 메소드의 실행 순서 지정
+ *  - OrderAnnotation인 경우 @Order(순서) 애노테이션을 사용해 순서
+ */
 @Slf4j
 @SpringBootTest
 @ActiveProfiles("test")
@@ -35,7 +48,8 @@ class UserServiceTest {
 
     @Test
     @Order(1)
-    void 새로운_사용자를_추가한다() {
+    @DisplayName("회원가입에 성공하면 DB에 저장된 유저의 정보를 리턴한다")
+    void testJoinNewUserAndReturnUserInfo() {
         User user = userService.join(email, name, password);
         assertThat(user, is(notNullValue()));
         assertThat(user.getId(), is(notNullValue()));
