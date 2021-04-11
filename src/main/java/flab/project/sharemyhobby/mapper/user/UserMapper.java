796523970c4c0868bc44c2a39801a79e3a5fe85a
Philip <pch4623@gmail.com>
2021-04-11 12:57:54 +0900

    [#5] 로그인 관련 매퍼 작성
    - 비밀번호 체크 전에 이메일로 유저 정보가 존재하는지 찾기 위한 쿼리 작성
    - 로그인 성공 시 마지막 로그인 시간 업데이트 쿼리 작성
    - Java LocalDateTime 타입과 매핑을 위해 typeHandler 라이브러리 및
      MyBatis 설정파일에 설정 추가

--- src/main/java/flab/project/sharemyhobby/mapper/user/UserMapper.java
+++ src/main/java/flab/project/sharemyhobby/mapper/user/UserMapper.java
@@ -1,13 +1,20 @@
 package flab.project.sharemyhobby.mapper.user;
 
+import flab.project.sharemyhobby.model.user.Email;
 import flab.project.sharemyhobby.model.user.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Mapper
 @Repository
 public interface UserMapper {
 
     long saveUser(User user);
+
+    void updateUser(User user);
+
+    Optional<User> findByEmail(Email email);
 
 }
