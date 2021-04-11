5bb072d62013745d8bf260001be97ac1e7782e56
Philip <pch4623@gmail.com>
2021-04-11 13:01:45 +0900

    [#5] 모델에 도메일 관련 로직 추가
    - 로그인 시 마지막 로그인 시간 업데이트를 위한 메서드 추가
    - MyBatis에서 유저 정보 조회 시 매핑을 위한 생성자 명시
    - Email 클래스 address 필드 불변성 보장을 위해 final 추가

--- src/main/java/flab/project/sharemyhobby/model/user/User.java
+++ src/main/java/flab/project/sharemyhobby/model/user/User.java
@@ -1,17 +1,16 @@
 package flab.project.sharemyhobby.model.user;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import flab.project.sharemyhobby.exception.InvalidPasswordException;
 import flab.project.sharemyhobby.model.address.Address;
 import flab.project.sharemyhobby.model.address.Location;
 import lombok.*;
 
 import java.time.LocalDateTime;
 
+import static java.time.LocalDateTime.now;
+
 @Getter
-@Builder(toBuilder=true)
-@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 @EqualsAndHashCode(of={"id", "email"})
 @ToString
 public class User {
@@ -42,5 +41,36 @@ public class User {
     private final LocalDateTime createAt;
 
     private LocalDateTime updateAt;
+
+    public void checkPassword(String inputPw, String userPw) {
+        if (!inputPw.equals(userPw))
+            throw new InvalidPasswordException();
+    }
+
+    public void loginPostProcess() {
+        lastLoginAt = now();
+    }
+
+    @Builder(toBuilder=true)
+    public User(Long id, @NonNull Email email, @NonNull String nickname, @NonNull String password, @NonNull Status status, Profile profile, Location location, Address address, LocalDateTime lastLoginAt, LocalDateTime createAt, LocalDateTime updateAt) {
+        this.id = id;
+        this.email = email;
+        this.nickname = nickname;
+        this.password = password;
+        this.status = status;
+        this.profile = profile;
+        this.location = location;
+        this.address = address;
+        this.lastLoginAt = lastLoginAt;
+        this.createAt = createAt;
+        this.updateAt = updateAt;
+    }
+
+    @Builder
+    public User(Long id, @NonNull Email email, LocalDateTime createAt) {
+        this.id = id;
+        this.email = email;
+        this.createAt = createAt;
+    }
 
 }
