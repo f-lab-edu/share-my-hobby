893334a4ac0c000459afb975dac032f0bf2a3147
Philip <pch4623@gmail.com>
2021-04-29 00:03:52 +0900

    [#8] 불필요한 메소드 체인 제거

--- src/main/java/flab/project/sharemyhobby/config/WebMvcConfig.java
+++ src/main/java/flab/project/sharemyhobby/config/WebMvcConfig.java
@@ -1,10 +1,14 @@
 package flab.project.sharemyhobby.config;
 
 import flab.project.sharemyhobby.interceptor.LoginCheckInterceptor;
+import flab.project.sharemyhobby.resolver.LoginUserIdResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -12,11 +16,18 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     private final LoginCheckInterceptor loginCheckInterceptor;
 
+    private final LoginUserIdResolver loginUserIdResolver;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginCheckInterceptor)
             .addPathPatterns("/users/**")
-            .excludePathPatterns("/users/join")
-            .excludePathPatterns("/users/login");
+            .excludePathPatterns("/users/join", "/users/login");
     }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserIdResolver);
+    }
+
 }
