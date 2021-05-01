2cab03a61663a0cef4d0eaccdad5f8cea1675827
Philip <pch4623@gmail.com>
2021-05-01 12:06:27 +0900

    [#10] 가독성 향상을 위해 커스텀 Exception에 개행 추가

--- src/main/java/flab/project/sharemyhobby/exception/LoginRequiredException.java
+++ src/main/java/flab/project/sharemyhobby/exception/LoginRequiredException.java
@@ -1,7 +1,9 @@
 package flab.project.sharemyhobby.exception;
 
 public class LoginRequiredException extends RuntimeException {
+
     public LoginRequiredException() {
         super("User has not yet logged in");
     }
+
 }
