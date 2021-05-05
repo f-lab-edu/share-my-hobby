f9dc5f51d870f5f02bde6a92655c5f0e103df32d
Philip <pch4623@gmail.com>
2021-05-06 00:15:25 +0900

    [#12] 리뷰반영: 이미지 업로드 실패 테스트 코드 작성
    - 빠른 테스트 실행을 위해 스프링 컨테이너에 의존하지 않는 단위 테스트로 변경
    - 예외 강제 발생 코드의 영향이 전파되지 않도록 테스트 인스턴스 라이프사이클을
      메소드 단위로 변경

--- src/test/java/flab/project/sharemyhobby/service/user/ProfileServiceTest.java
+++ src/test/java/flab/project/sharemyhobby/service/user/ProfileServiceTest.java
@@ -1,13 +1,16 @@
 package flab.project.sharemyhobby.service.user;
 
-import flab.project.sharemyhobby.aws.S3Uploader;
-import flab.project.sharemyhobby.model.user.Email;
+import flab.project.sharemyhobby.exception.FileUploadException;
+import flab.project.sharemyhobby.mapper.user.ProfileMapper;
 import flab.project.sharemyhobby.model.user.Profile;
 import flab.project.sharemyhobby.model.user.User;
+import flab.project.sharemyhobby.util.FileUploader;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,18 +21,23 @@ import java.io.IOException;
 import java.net.URL;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.ThrowableAssert.catchThrowable;
+import static org.mockito.Mockito.*;
 
 @Slf4j
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 @ActiveProfiles("test")
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class ProfileServiceTest {
 
-    @Autowired
-    private ProfileService profileService;
+    @Mock
+    private ProfileMapper profileMapper;
 
-    @Autowired
-    private UserService userService;
+    @Mock
+    private FileUploader fileUploader;
+
+    @InjectMocks
+    private ProfileService profileService;
 
     private MultipartFile profileImage;
 
@@ -41,24 +49,35 @@ class ProfileServiceTest {
         return new MockMultipartFile(fileName, new FileInputStream(testImage));
     }
 
-    @BeforeAll
+    @BeforeEach
     void setUp() throws IOException {
         profileImage = getImageFile("/test_profile.jpg");
-        user = userService.join(new Email("adorno10@naver.com"), "cold-pumpkin", "12345678");
     }
 
     @Test
     @DisplayName("프로필을 등록하면 DB에 저장된 프로필 정보를 리턴한다")
-    void testRegisterProfileAndReturnProfileInfo() {
-        String statusMessage = "아..배고프다";
+    void testRegisterProfileAndReturnProfileInfo() throws IOException {
+        String statusMessage = "반갑습니다!";
         Profile profile = profileService.registerProfile(1L, profileImage, statusMessage);
 
         assertThat(profile.getId()).isNotNull();
         assertThat(profile.getUserId()).isEqualTo(1L);
-        assertThat(profile.getProfileImageUrl()).isNotNull();
         assertThat(profile.getStatusMessage()).isEqualTo(statusMessage);
+    }
 
-        log.info("프로필 등록 : {}", profile);
+    @Test
+    @DisplayName("프로필을 등록을 실패하면 FileUploadException 발생시킨다")
+    void testThrowFileUploadExceptionIfProfileRegisterFail() throws IOException {
+        doThrow(FileUploadException.class)
+                .when(fileUploader)
+                .upload(profileImage);
+
+        Throwable thrown = catchThrowable(() -> {
+            profileService.registerProfile(1L, profileImage, "아 배고파");
+        });
+
+        verify(fileUploader).upload(profileImage);
+        assertThat(thrown).isInstanceOf(FileUploadException.class);
     }
 
 }
