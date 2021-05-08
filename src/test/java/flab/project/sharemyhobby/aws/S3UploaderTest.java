5b02647252d56b4496e9f22664f927137dd6e38a
Philip <pch4623@gmail.com>
2021-05-08 15:15:12 +0900

    [#14] AWS S3 파일 삭제 기능 테스트 코드

--- src/test/java/flab/project/sharemyhobby/aws/S3UploaderTest.java
+++ src/test/java/flab/project/sharemyhobby/aws/S3UploaderTest.java
@@ -10,6 +10,8 @@ import software.amazon.awssdk.auth.credentials.AwsCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
+import software.amazon.awssdk.services.s3.model.S3Exception;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -17,12 +19,30 @@ import java.io.IOException;
 import java.net.URL;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class S3UploaderTest {
 
     private FileUploader fileUploader;
+
+    private MultipartFile profileImage;
+
+    private MultipartFile getTestImageFile() {
+        String fileName = "/test_profile.jpg";
+        URL testProfileUrl = getClass().getResource(fileName);
+        assertThat(testProfileUrl).isNotNull();
+
+        MultipartFile testProfileImage = null;
+        try {
+            File file = new File(testProfileUrl.getFile());
+            testProfileImage = new MockMultipartFile(fileName, "test_profile.jpg", "image/jpg", new FileInputStream(file));
+        } catch (IOException e) {
+            log.error("Failed to read test image : {}", e.getMessage());
+        }
+        return testProfileImage;
+    }
 
     @BeforeAll
     void setUp() {
@@ -38,23 +58,26 @@ class S3UploaderTest {
                     .build();
 
         fileUploader = new S3Uploader(s3Client, bucketName);
+
+        profileImage = getTestImageFile();
     }
 
     @Test
     @DisplayName("AWS S3 버킷으로 프로필 이미지를 업로드를 성공하면 이미지 URL을 리턴한다")
     void testUploadProfileImageToS3AndReturnImageUrl() throws IOException {
-        String fileName = "/test_profile.jpg";
-        URL testProfile = getClass().getResource(fileName);
-
-        assertThat(testProfile).isNotNull();
-
-        File file = new File(testProfile.getFile());
-        FileInputStream input = new FileInputStream(file);
-        MultipartFile profileImage = new MockMultipartFile(fileName, input);
         String url = fileUploader.upload(profileImage);
 
         assertThat(url).isNotNull();
         log.info("S3 bucket url: {}", url);
+    }
+
+    @Test
+    @DisplayName("AWS S3에서 파일 삭제 후 HEAD 요청을 하면 NoSuchKeyException을 던진다")
+    void test() {
+        fileUploader.delete(profileImage.getOriginalFilename());
+        assertThatThrownBy(() -> fileUploader.head(profileImage.getOriginalFilename()))
+                .isExactlyInstanceOf(NoSuchKeyException.class)
+                .isInstanceOf(S3Exception.class);
     }
 
 }
