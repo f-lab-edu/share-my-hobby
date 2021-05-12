cdb089ae525e5f10705fe06f4ab5364cbdee49aa
Philip <pch4623@gmail.com>
2021-05-12 22:54:23 +0900

    [#18] 리뷰반영: 예외클래스 내용 오류 수정

--- src/main/java/flab/project/sharemyhobby/exception/ProfileNotFoundException.java
+++ src/main/java/flab/project/sharemyhobby/exception/ProfileNotFoundException.java
@@ -1,7 +1,7 @@
 package flab.project.sharemyhobby.exception;
 
 public class ProfileNotFoundException extends RuntimeException {
-
+    
     public ProfileNotFoundException() {
         super("User profile not found");
     }
