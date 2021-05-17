a83eb5ce8042b3cb21290afa720d8c4a867025c9
Philip <pch4623@gmail.com>
2021-05-17 18:33:43 +0900

    [#22] 취미 데이터를 담을 모델 생성

---
+++ src/main/java/flab/project/sharemyhobby/model/hobby/Hobby.java
@@ -0,0 +1,15 @@
+package flab.project.sharemyhobby.model.hobby;
+
+import lombok.*;
+
+@Getter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class Hobby {
+
+    final private Integer id;
+
+    private String hobbyName;
+
+}
