3afb52dacb33167a711a8666f6194104cfcb3484
Philip <pch4623@gmail.com>
2021-06-01 23:35:44 +0900

    [#31] 동네이름으로 주소찾기 컨트롤러 작성

---
+++ src/main/java/flab/project/sharemyhobby/controller/address/AddressRestController.java
@@ -0,0 +1,26 @@
+package flab.project.sharemyhobby.controller.address;
+
+import flab.project.sharemyhobby.model.address.Address;
+import flab.project.sharemyhobby.model.api.request.address.AddressRequest;
+import flab.project.sharemyhobby.service.address.AddressService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/addresses")
+public class AddressRestController {
+
+    private final AddressService addressService;
+
+    @GetMapping("/address-list")
+    public List<Address> search(@RequestBody AddressRequest addressRequest) {
+        return addressService.getAddressList(addressRequest);
+    }
+
+}
