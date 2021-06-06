26c84174bbedf770b885596fac8ad185d9304531
Philip <pch4623@gmail.com>
2021-06-06 12:20:11 +0900

    [#35] 유저 주소 정보 저장 시 발생할 수 있는 예외 구현

--- src/main/java/flab/project/sharemyhobby/exception/address/AddressCountOverException.java
+++ src/main/java/flab/project/sharemyhobby/exception/address/AddressCountOverException.java
@@ -5,9 +5,9 @@ import flab.project.sharemyhobby.model.address.Address;
 import java.text.MessageFormat;
 import java.util.List;
 
-public class LimitAddressCountOverException extends RuntimeException {
+public class AddressCountOverException extends RuntimeException {
 
-    public LimitAddressCountOverException(long  userId, List<Address> addresses) {
+    public AddressCountOverException(long  userId, List<Address> addresses) {
         super(MessageFormat.format("User id [{0}] has already registered maximun number of addresses : {1}", userId, addresses));
     }
 
