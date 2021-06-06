502cc75402e16e9a16fec6e381d60c84a63984ac
Philip <pch4623@gmail.com>
2021-06-06 12:18:34 +0900

    [#35] 유저 주소 저장을 위한 서비스 구현
    - 이미 등록 가능한 모든 주소를 저장한 경우 예외 발생
    - 이미 같은 주소를 등록한 경우 예외 발생

--- src/main/java/flab/project/sharemyhobby/service/address/AddressService.java
+++ src/main/java/flab/project/sharemyhobby/service/address/AddressService.java
@@ -1,11 +1,16 @@
 package flab.project.sharemyhobby.service.address;
 
 import flab.project.sharemyhobby.api.kakao.KakaoRestApiHelper;
+import flab.project.sharemyhobby.exception.address.AddressCountOverException;
+import flab.project.sharemyhobby.exception.hobby.DuplicationAddressException;
+import flab.project.sharemyhobby.mapper.address.AddressMapper;
 import flab.project.sharemyhobby.model.address.Address;
 import flab.project.sharemyhobby.model.api.request.address.AddressRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,8 +22,26 @@ public class AddressService {
 
     private final KakaoRestApiHelper kakaoRestApiHelper;
 
+    private final AddressMapper addressMapper;
+
+    private static final int MAX_USER_ADDRESS_CNT = 2;
+
+
     public List<Address> getAddressList(AddressRequest addressRequest) {
         return kakaoRestApiHelper.getAddressByTownName(addressRequest);
+    }
+
+    @Transactional
+    public void registerAddress(Address address, long userId) {
+        List<Address> userAddressList = addressMapper.findUserAddressByUserId(userId);
+        if (userAddressList.size() >= MAX_USER_ADDRESS_CNT)
+            throw new AddressCountOverException(userId, userAddressList);
+
+        try {
+            addressMapper.saveUserAddress(userId, address);
+        } catch (DuplicateKeyException e) {
+            throw new DuplicationAddressException(userId, address);
+        }
     }
 
 }
